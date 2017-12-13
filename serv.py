b44a291e0f6ea1dc13a8c5c40714815601b3dde6
Nick Dima <ncd2dq@gmail.com>
2017-12-13 14:25:15 -0500

    built out server

--- serv.py
+++ serv.py
@@ -8,15 +8,33 @@ then deliver all messages when a client asks for a message
 HOST = ('127.0.0.1', 5000)
 
 class Server(object):
-    def __init__(self, host):
+    def __init__(self, host, secret='recieve_all'):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind(host)
         self.s.listen(1)
         self.connections = []
-        self.message = []
+        self.messages = []
+        self.secret = secret
 
-    def _message_server(self, conn_tuple):
+    def send_que(self, conn):
+
+    def _message_server(self, conn_tuple, buffer=1024):
         '''This will handle storing data'''
+        while True:
+            try:
+                msg = conn_tuple[0].recv(buffer)
+                if str(msg, 'utf-8') == self.secret:
+                    for conn, addr in self.connections:
+                        for message in self.messages:
+                            conn.send(message)
+                    self.messages = []
+                else:
+                    self.messages.append(msg)
+
+            except Exception as e:
+                print(e)
+                self.connections.remove(conn_tuple[0])
+                break
 
     def _connection_manager(self):
         '''This will manage putting new connections in list and sending them to the message server'''
@@ -29,4 +47,5 @@ class Server(object):
             chat_thread = threading.Thread(target=self._message_server, args=(connect_tuple,))
             chat_thread.daemon = True
             chat_thread.start()
+
 
